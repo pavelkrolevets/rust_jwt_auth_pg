be3105a7e20e38f2e289f332e40a0b0d862eac8c
pavelkrolevets <pavelkrolevets@gmail.com>
2019-09-20 18:22:51 +0800

    init

---
+++ src/models.rs
@@ -0,0 +1,13 @@
+use super::schema::people;
+
+#[derive(Queryable, AsChangeset, Serialize, Deserialize)]
+#[table_name = "people"]
+
+pub struct Person {
+    pub id: i32,
+    pub first_name: String,
+    pub last_name: String,
+    pub age: i32,
+    pub profession: String,
+    pub salary: i32,
+}
