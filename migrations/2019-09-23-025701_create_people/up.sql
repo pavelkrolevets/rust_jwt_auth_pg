0032a8d49f8835105c46d8dc305fedbbcc346f91
pavelkrolevets <pavelkrolevets@gmail.com>
2019-09-23 15:25:49 +0800

    added diesel db

--- migrations/2019-09-23-025701_create_people/up.sql
+++ migrations/2019-09-23-025701_create_people/up.sql
@@ -1,9 +1,15 @@
 CREATE TABLE people(
 
   id SERIAL PRIMARY KEY,
+
   first_name VARCHAR NOT NULL,
+
   last_name VARCHAR NOT NULL,
+
   age INT NOT NULL,
+
   profession VARCHAR NOT NULL,
+
   salary INT NOT NULL
+
 )
